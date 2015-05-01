1925e90970891b81610d7d8b3ee2551e2c00e9b0
bav735 <bav735@mail.ru>
2015-05-01 16:56:32 +0300

    Added MyList

--- BSTreeAnyType.hs
+++ BSTreeAnyType.hs
@@ -1,13 +1,13 @@
 module BSTreeAnyType where --BSTreeAnyType.hs
 
-data Tree a = Any a (Tree a) (Tree a) | Nil deriving (Show, Eq)
+data Tree a = Val a (Tree a) (Tree a) | Nil deriving (Show, Eq)
 
 tHeight :: Tree a -> Int
 tHeight t = case t of
   Nil -> 0
-  Any a left right -> ((max (tHeight left) (tHeight right)) + 1)  
+  Val a left right -> (max (tHeight left) (tHeight right)) + 1
 
 tMap :: Tree a -> (a -> b) -> Tree b
 tMap t f = case t of
   Nil -> Nil
-  Any a left right -> Any (f a) (tMap left f) (tMap right f)
+  Val a left right -> Val (f a) (tMap left f) (tMap right f)
---
+++ MyList.hs
@@ -0,0 +1,15 @@
+module MyList where --MyList.hs
+
+data List a b = Nil | Head a (List b a)  deriving (Show, Eq)
+
+--длина списка
+size :: List a b -> Int
+size list = case list of
+        Nil -> 0
+        Head a tail -> 1 + (size tail)
+
+--преобрзование списка по двум функциям
+dMap :: List a b -> (a -> c) -> (b -> d) -> List c d
+dMap list f1 f2 = case list of
+        Nil -> Nil
+        Head a tail -> Head (f1 a) (dMap tail f2 f1)
