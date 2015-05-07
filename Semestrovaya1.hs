34b5b00597f6cf466f610047d7e1a6b297a89577
bav735 <bav735@mail.ru>
2015-05-07 19:50:50 +0300

    Added Semestrovaya1

---
+++ Semestrovaya1.hs
@@ -0,0 +1,34 @@
+module Semestrovaya1 where --Semestrovaya1.hs  
+   
+type Var a = a
+data Term = Var String | App Term Term | Abs (Var String) Term deriving (Eq, Ord, Read)
+instance Show Term where
+   show (Var x) = x
+   show (Abs x t) = "\"" ++ x ++ "." ++ show t
+   show (App t1 t2) = "(" ++ show t1 ++ " " ++ show t2 ++ ")"
+--for testing
+t1::Term
+t1 = Abs ("x") (App (Var "x") (Var "y"))
+t2::Term
+t2 = Var "z"
+t3::Term
+t3 = App t1 t2
+
+replaceVar :: Term -> Var String -> Term -> Term
+replaceVar t0 x t = case t0 of      
+   Var y -> if y == x
+      then t
+      else t0
+   Abs y t1 -> if x == y   
+			then t0  
+			else Abs y (replaceVar t1 x t)
+   App t1 t2 -> App (replaceVar t1 x t) (replaceVar t2 x t)      
+
+eval :: Term -> Term
+eval t0 = case t0 of
+   Abs x t -> Abs x (eval t)
+   App t1 t2 ->  case t1 of      
+      Abs x t -> eval(replaceVar t x t2)	
+      _ -> App (eval t1) (eval t2)      
+   _ -> t0
+
