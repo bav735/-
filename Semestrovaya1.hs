5403f82606832b6a4643ed0f95929dcf19532ea0
bav735 <bav735@mail.ru>
2015-05-10 21:36:36 +0300

    Added Semestrovaya2

--- Semestrovaya1.hs
+++ Semestrovaya1.hs
@@ -1,19 +1,16 @@
 module Semestrovaya1 where --Semestrovaya1.hs  
-   
+
+--variable representation
 type Var a = a
+
+--term representation
 data Term = Var String | App Term Term | Abs (Var String) Term deriving (Eq, Ord, Read)
 instance Show Term where
    show (Var x) = x
    show (Abs x t) = "\"" ++ x ++ "." ++ show t
    show (App t1 t2) = "(" ++ show t1 ++ " " ++ show t2 ++ ")"
---for testing
-t1::Term
-t1 = Abs ("x") (App (Var "x") (Var "y"))
-t2::Term
-t2 = Var "z"
-t3::Term
-t3 = App t1 t2
 
+--substitution function
 replaceVar :: Term -> Var String -> Term -> Term
 replaceVar t0 x t = case t0 of      
    Var y -> if y == x
@@ -24,6 +21,7 @@ replaceVar t0 x t = case t0 of
 			else Abs y (replaceVar t1 x t)
    App t1 t2 -> App (replaceVar t1 x t) (replaceVar t2 x t)      
 
+--evaluation function
 eval :: Term -> Term
 eval t0 = case t0 of
    Abs x t -> Abs x (eval t)
@@ -31,4 +29,11 @@ eval t0 = case t0 of
       Abs x t -> eval(replaceVar t x t2)	
       _ -> App (eval t1) (eval t2)      
    _ -> t0
-
+   
+--for testing
+t1::Term
+t1 = Abs ("x") (App (Var "x") (Var "y"))
+t2::Term
+t2 = Var "z"
+t3::Term
+t3 = App t1 t2
---
+++ Semestrovaya2.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE GADTs, KindSignatures, DataKinds, FlexibleInstances, StandaloneDeriving,
+NoMonomorphismRestriction, FlexibleContexts, ViewPatterns, OverlappingInstances, ScopedTypeVariables #-}
+module Semestrovaya2 where --Semestrovaya2.hs
+
+--type representation
+data Type = 
+     T String
+   | Arr Type Type
+   deriving Eq 
+instance Show (Type) where
+   show (T s) = s
+   show (Arr t1 t2) = 
+      if t1 == t2 then show t1
+      else "["++show t1++"=>"++show t2++"]"
+           
+--term representation
+data Term =
+     Var String Type
+   | App Term Term Type
+   | Abs String Type Term Type
+   | Invalid String
+   deriving Eq
+instance Show (Term) where
+   show (te0) = if isValidTerm te0 then case te0 of  
+      Var s t -> s ++ ":" ++ show t
+      App te1 te2 ty -> "(" ++ show te1 ++ "->" ++ show te2 ++ "):" ++ show ty         
+      Abs x tyx te tyr -> "(\\" ++ show(Var x tyx) ++ "." ++ show te ++ "):" ++ show tyr
+   else case te0 of
+      Invalid err -> err
+      _ -> "Invalid term!"
+   
+      
+isValidTerm :: Term -> Bool
+isValidTerm te0 = case te0 of
+   Var s t -> True
+   App te1 te2 ty -> case getTermType te1 of
+      Arr ty1 ty2 -> if ty1 == getTermType te2 && ty2 == ty
+         then isValidTerm te1 && isValidTerm te2
+         else False
+      _ -> False
+   Abs x tyx te tyr -> case tyr of
+      Arr ty1 ty2 -> if ty1 == tyx && ty2 == getTermType te
+         then isValidTerm te
+         else False
+      _ -> False
+   _ -> False
+ 
+getTermType :: Term -> Type
+getTermType te0 = case te0 of      
+   Var y t -> t
+   Abs y tyy te tyr -> tyr
+   App te2 te3 tyr -> tyr
+     
+--substitution function
+replaceVar :: Term -> String -> Type -> Term -> Term
+replaceVar te0 x tyx te1 = case te0 of      
+   Var y t -> if y == x
+      then te1
+      else te0
+   Abs y tyy te tyr -> if x == y   
+			then te0  
+			else Abs y tyy (replaceVar te x tyx te1) tyr
+   App te2 te3 tyr -> App (replaceVar te2 x tyx te1) (replaceVar te3 x tyx te1) tyr   
+
+--evaluation function
+eval :: Term -> Term
+eval te0 = if isValidTerm te0
+   then case te0 of
+      Abs x tyx te tyr -> Abs x tyx (eval te) tyr
+      App te1 te2 tyr ->  case te1 of      
+         Abs x tyx te tyr -> eval(replaceVar te x tyx te2)	
+         _ -> App (eval te1) (eval te2) tyr
+      _ -> te0
+   else Invalid "Couldn't evaluate - invalid term!"
+     
+--for testing
+ty::Type
+ty = Arr (T "Cat") (T "Dog") --type
+te::Term
+tred::Term
